8c0d678f87ad2283aa0bc08d5a285e836af3c517
Steven <Steven.A.Johnson-1@ou.edu>
2019-03-26 15:56:29 -0500

    second commit, skeleton of BoardGame.java complete, time to actually write stuff

---
+++ BoardGame.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class BoardGame
+{
+	public BoardGame()
+	{
+		
+	}
+	
+	public void addPlayer(String PlayerName, GamePiece gamepiece)
+	{
+		
+	}
+	
+	public GamePiece getPlayerGamePiece(String name)
+	{
+		GamePiece gp = new GamePiece();
+		return gp;
+	}
+	
+	public String getPlayerWithGamePiece(GamePiece gamepiece)
+	{
+		return "";
+	}
+	
+	public void movePlayer(String playerName, Location newLocation)
+	{
+		
+	}
+	
+	public void moveTwoPlayers(String[] playerNames, Location[] newLocations)
+	{
+		
+	}
+	
+	public Location getPlayersLocation(String playerName)
+	{
+		
+	}
+	
+	public ArrayList<String> getPlayersAtLocation()
+	{
+		
+	}
+	
+	public ArrayList<GamePieces> getGamePiecesAtLocation(Location loc)
+	{
+		
+	}
+	
+	public Set<String> getPlayers()
+	{
+		
+	}
+	
+	
+	public Set<Location> getPlayerLocations()
+	{
+		
+	}
+	
+	public Set<GamePiece> getPlayerPieces()
+	{
+		
+	}
+}
